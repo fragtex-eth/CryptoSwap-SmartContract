df8fae0b6266602cc96204a20fb4f95014924318
Lukas Ehlers <ehlers@outson.com>
2023-01-03 18:46:03 +0100

    updating test cases

--- test/unit/01-CPAMM.test.js
+++ test/unit/01-CPAMM.test.js
@@ -25,20 +25,20 @@ let creditB = async function (to, amount) {
         tokenA = await ethers.getContract("TokenA");
         tokenB = await ethers.getContract("TokenB");
 
-        tokenContract = tokenContract.connect(deployer);
-        tokenContractAlice = tokenContract.connect(alice);
-        tokenContractBob = tokenContract.connect(bob);
-        tokenContractCharles = tokenContract.connect(charles);
+        tokenContract = await tokenContract.connect(deployer);
+        tokenContractAlice = await tokenContract.connect(alice);
+        tokenContractBob = await tokenContract.connect(bob);
+        tokenContractCharles = await tokenContract.connect(charles);
 
-        tokenContractDeployerA = tokenA.connect(deployer);
-        tokenContractAliceA = tokenA.connect(alice);
-        tokenContractBobA = tokenA.connect(bob);
-        tokenContractCharlesA = tokenA.connect(charles);
+        tokenContractDeployerA = await tokenA.connect(deployer);
+        tokenContractAliceA = await tokenA.connect(alice);
+        tokenContractBobA = await tokenA.connect(bob);
+        tokenContractCharlesA = await tokenA.connect(charles);
 
-        tokenContractDeployerB = tokenB.connect(deployer);
-        tokenContractAliceB = tokenB.connect(alice);
-        tokenContractBobB = tokenB.connect(bob);
-        tokenContractCharlesB = tokenB.connect(charles);
+        tokenContractDeployerB = await tokenB.connect(deployer);
+        tokenContractAliceB = await tokenB.connect(alice);
+        tokenContractBobB = await tokenB.connect(bob);
+        tokenContractCharlesB = await tokenB.connect(charles);
       });
       describe("CPAMM", function () {
         describe("Initalization()", function () {
@@ -145,9 +145,51 @@ let creditB = async function (to, amount) {
                 .reverted;
             });
           });
+          describe("Share Balance Change", function () {
+            it("Balance updates correctly after adding liquidity", async function () {
+              await creditA(alice, 1000);
+              await creditB(alice, 1000);
+              await tokenContractAliceB.approve(
+                tokenContract.address,
+                ethers.utils.parseUnits(`${1000000000000}`, 18)
+              );
+              await tokenContractAliceA.approve(
+                tokenContract.address,
+                ethers.utils.parseUnits(`${1000000000000}`, 18)
+              );
+              await tokenContractAlice.addLiquidity(100, 100);
+              expect(
+                await tokenContractAlice.balanceOf(alice.address)
+              ).to.be.equal(100);
+              expect(await tokenContractBob.balanceOf(bob.address)).to.be.equal(
+                0
+              );
+            });
+            it("Balance updates correctly after removing liquidity", async function () {
+              await creditA(alice, 1000);
+              await creditB(alice, 1000);
+              await tokenContractAliceB.approve(
+                tokenContract.address,
+                ethers.utils.parseUnits(`${1000000000000}`, 18)
+              );
+              await tokenContractAliceA.approve(
+                tokenContract.address,
+                ethers.utils.parseUnits(`${1000000000000}`, 18)
+              );
+              await tokenContractAlice.addLiquidity(100, 100);
+              expect(
+                await tokenContractAlice.balanceOf(alice.address)
+              ).to.be.equal(100);
+              await tokenContractAlice.removeLiquidity(7);
+              expect(
+                await tokenContractAlice.balanceOf(alice.address)
+              ).to.be.equal(93);
+            });
+          });
         });
-        describe("BalanceChange", function () {
-          it("Balance updates correctly after adding liquidity", async function () {
+        describe("Swap()", function () {
+          beforeEach(async () => {
+            //Adding Liquidity Alice & Bob
             await creditA(alice, 1000);
             await creditB(alice, 1000);
             await tokenContractAliceB.approve(
@@ -158,13 +200,78 @@ let creditB = async function (to, amount) {
               tokenContract.address,
               ethers.utils.parseUnits(`${1000000000000}`, 18)
             );
-            await tokenContractAlice.addLiquidity(100, 100);
-            expect(
-              await tokenContractAlice.balanceOf(alice.address)
-            ).to.be.equal(100);
-            expect(await tokenContractBob.balanceOf(bob.address)).to.be.equal(
-              0
+            await tokenContractAlice.addLiquidity(1000, 1000);
+            await creditA(bob, 1000);
+            await creditB(bob, 1000);
+            await tokenContractBobB.approve(
+              tokenContract.address,
+              ethers.utils.parseUnits(`${1000000000000}`, 18)
             );
+            await tokenContractBobA.approve(
+              tokenContract.address,
+              ethers.utils.parseUnits(`${1000000000000}`, 18)
+            );
+            await tokenContractBob.addLiquidity(250, 250);
+          });
+          it("Swap is not possible if token not approved", async function () {
+            await expect(
+              tokenContractCharles.swap(tokenA.address, 10)
+            ).to.be.revertedWith("ERC20: insufficient allowance");
+          });
+          it("Swap is not possible if token not enough balance", async function () {
+            await tokenContractCharlesA.approve(
+              tokenContract.address,
+              ethers.utils.parseUnits(`${1000000000000}`, 18)
+            );
+            await expect(
+              tokenContractCharles.swap(tokenA.address, 10)
+            ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
+          });
+          it("Swap successfully and correctly executed if prerequisit is met correct", async function () {
+            await creditA(charles, 1000);
+            await tokenContractCharlesA.approve(
+              tokenContract.address,
+              ethers.utils.parseUnits(`${1000000000000}`, 18)
+            );
+            let balanceA1 = await tokenContractDeployerA.balanceOf(
+              charles.address
+            );
+            let balanceB1 = await tokenContractDeployerB.balanceOf(
+              charles.address
+            );
+            await tokenContractCharles.swap(tokenA.address, 100);
+            let balanceA2 = await tokenContractDeployerA.balanceOf(
+              charles.address
+            );
+            let balanceB2 = await tokenContractDeployerB.balanceOf(
+              charles.address
+            );
+
+            expect(balanceA1).to.equal(balanceA2.add(100));
+            expect(balanceB1).to.equal(balanceB2.sub(91)); //Slippage
+          });
+          it("Removed liquidity balance updates correctly after swap", async function () {
+            await creditA(charles, 1000);
+            await tokenContractCharlesA.approve(
+              tokenContract.address,
+              ethers.utils.parseUnits(`${1000000000000}`, 18)
+            );
+            await tokenContractCharles.swap(tokenA.address, 100);
+            let balanceAbefore = await tokenContractDeployerA.balanceOf(
+              alice.address
+            );
+            let balanceBbefore = await tokenContractDeployerB.balanceOf(
+              alice.address
+            );
+            await tokenContractAlice.removeLiquidity(100);
+            let balanceAafter = await tokenContractDeployerA.balanceOf(
+              alice.address
+            );
+            let balanceBafter = await tokenContractDeployerB.balanceOf(
+              alice.address
+            );
+            expect(balanceAbefore).to.equal(balanceAafter.sub(108));
+            expect(balanceBbefore).to.equal(balanceBafter.sub(92)); //Slippage
           });
         });
       });
